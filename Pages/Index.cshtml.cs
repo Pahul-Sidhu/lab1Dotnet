75ec5b6ca65cb788e5513175da358375ca33404a
Pahul Sidhu <pahulsidhu@Pahuls-MacBook-Pro.local>
2023-09-19 17:47:27 -0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using lab1.Models;
+
+namespace lab1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public int Year { get; set; }
+
+        [TempData]
+        public required string ZodiacSign { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ZodiacSign = Utils.GetZodiac(Year);
+                return Page();
+            }
+            return BadRequest();
+        }
+    }
+}
